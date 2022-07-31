a1a168dc3208fdcb0b29412298931b02550132c8
RWGregory <91198091+RWGregory@users.noreply.github.com>
2022-07-30 21:02:43 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import {marked} from 'marked'
+
+marked.setOptions({
+  breaks: true 
+})
+
+const renderer = new marked.Renderer()
+renderer.link = (href, title, text) => {
+  return `<a target="_blank" href="${href}">${text}</a>`
+}
+const defaultText = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+`
 
 function App() {
+  const [markdown, setMarkdown] = useState(defaultText)
+
+  const handleChange = (e) => {
+    setMarkdown(e.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea rows={20} cols={150} id="editor" onChange={handleChange} type="text" value={markdown}>
+        {defaultText}
+      </textarea>
+      <div className='container'>
+        <div dangerouslySetInnerHTML={{
+          __html: marked(markdown, {renderer})
+    }} id ="preview"></div>
+
+      </div>
     </div>
   );
 }
